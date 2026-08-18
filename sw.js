7bd6ca506a7b6034e250979287640f37a46767cd
khlilsh2017-creator <khlilsh2017@gmail.com>
2026-08-18 04:25:15 +0300

    تعديل كذا

--- sw.js
+++ sw.js
@@ -2,10 +2,10 @@
 // 🏢 Service Worker - نظام ابن مختار (الإصدار النهائي v19)
 // ============================================================
 
-const CACHE_NAME = 'ibn-mukhtar-pos-v23';
-const STATIC_CACHE = 'ibn-mukhtar-static-v23';
-const DYNAMIC_CACHE = 'ibn-mukhtar-dynamic-v23';
-const VERSION = '2025-02-17-009';
+const CACHE_NAME = 'ibn-mukhtar-pos-v24';
+const STATIC_CACHE = 'ibn-mukhtar-static-v24';
+const DYNAMIC_CACHE = 'ibn-mukhtar-dynamic-v24';
+const VERSION = '2025-02-17-010';
 
 const STATIC_ASSETS = [
     '/',
