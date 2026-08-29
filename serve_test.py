f43c75f44c2e9c49903cc1712e97651a06bb707b
khlilsh2017-creator <khlilsh2017@gmail.com>
2026-08-29 05:07:56 +0300

    تعديل كذا

--- serve_test.py
+++
@@ -1,9 +0,0 @@
-from http.server import ThreadingHTTPServer, SimpleHTTPRequestHandler
-from pathlib import Path
-
-root = Path(__file__).parent
-class Handler(SimpleHTTPRequestHandler):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, directory=str(root), **kwargs)
-
-ThreadingHTTPServer(('0.0.0.0', 8765), Handler).serve_forever()
